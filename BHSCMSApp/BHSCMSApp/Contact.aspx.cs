6e4a4d1ca0336c87dadf3d35c0d0b9b37e2542b9
miniwheatsnb <miniwheatsnb@gmail.com>
2014-11-18 18:27:08 -0500

    Fixed sending Email.
    Will implement this to the petition page

--- BHSCMSApp/BHSCMSApp/Contact.aspx.cs
+++ BHSCMSApp/BHSCMSApp/Contact.aspx.cs
@@ -19,25 +19,34 @@ namespace BHSCMSApp
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
 
+            string email = txtEmail.Text;
+            string name = txtName.Text;
+            string message = txtMessage.Text;
+           
+
             try
             {
+                
+
                 //Create the msg object to be sent
                 MailMessage msg = new MailMessage();
                 //Add your email address to the recipients
-                 msg.To.Add("runewinja@gmail.com");
+                 msg.To.Add("miniwheatsnb@gmail.com");
                 //Configure the address we are sending the mail from
-                MailAddress address = new MailAddress(txtEmail.Text);
+                MailAddress address = new MailAddress(email);
                 msg.From = address;
                 //Append their name in the beginning of the subject
-                msg.Subject = txtName.Text + " :  " + ddlSubject.Text;
-                msg.Body = txtMessage.Text;
+                msg.Subject = name + " :  " + ddlSubject.Text;
+                msg.Body = message;
 
                 //Configure an SmtpClient to send the mail.
                 SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
                 client.EnableSsl = true; //only enable this if your provider requires it
                 ////Setup credentials to login to our sender email address ("UserName", "Password")
-                NetworkCredential credentials = new NetworkCredential("runewinja@gmail.com", "jarrete2014");
-                client.Credentials = credentials;
+                client.Credentials = new System.Net.NetworkCredential("miniwheatsnb@gmail.com", "34kannon34");
+                //NetworkCredential credentials = new NetworkCredential("miniwheatsnb@gmail.com", "34kannon34");
+                //client.Credentials = credentials;
+
 
                 //Send the msg
                 client.Send(msg);
@@ -46,8 +55,8 @@ namespace BHSCMSApp
                 lblResult.Text = "Your message was sent!";
 
                 //Clear the form
-                txtName.Text = "";
-                txtMessage.Text = "";
+                //txtName.Text = "";
+                //txtMessage.Text = "";
             }
             catch
             {
