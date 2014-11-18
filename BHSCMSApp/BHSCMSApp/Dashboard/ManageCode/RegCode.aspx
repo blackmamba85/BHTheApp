cea9599575d196e5de67d5c7afc385bb49ac30bb
miniwheatsnb <miniwheatsnb@gmail.com>
2014-11-18 17:02:10 -0500

    Created Modify RegCode Tab & codebehind

---
+++ BHSCMSApp/BHSCMSApp/Dashboard/ManageCode/RegCode.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dashboard/DashboardAdmin.Master" AutoEventWireup="true" CodeBehind="RegCode.aspx.cs" Inherits="BHSCMSApp.Dashboard.ManageCode.RegCode" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <div class="col-md-8">
+         <h3 class="headers">Manage Code</h3> 
+          <hr />
+
+
+        <div class="align-center">
+            <asp:Label runat="server" AssociatedControlID="currentCode" CssClass="col-md-4">Current Registration Code</asp:Label>
+            <div class="align-center">
+                <asp:TextBox runat="server" ID="currentCode" CssClass="form-control" ReadOnly="true" BackColor="Gray" style="color:black; font:bold;" />
+            </div>
+        </div>
+
+
+
+        <div class="align-center">
+            <asp:Label runat="server" AssociatedControlID="regCode" CssClass="col-md-4">Enter New Registration Code</asp:Label>
+            <div class="align-center">
+                <asp:TextBox runat="server" ID="regCode" MaxLength="10" CssClass="form-control" />
+                <asp:RequiredFieldValidator runat="server" ControlToValidate="regCode" ValidationGroup="modify"
+                    CssClass="text-danger" ErrorMessage="The Registration Code is required." />
+            </div>
+        </div>
+
+
+        <div class="align-center">
+            <div class="align-center">
+            <asp:Button runat="server" OnClick="submitCode_Click" Text="Submit" Width="40%" CssClass="btn btn-info" CausesValidation="true" ValidationGroup="modify" />
+            </div>
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+</asp:Content>
---
+++ BHSCMSApp/BHSCMSApp/Dashboard/ManageCode/RegCode.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BHSCMSApp.Dashboard.ManageCode
+{
+    public partial class RegCode : System.Web.UI.Page
+    {
+
+        string connString = ConfigurationManager.ConnectionStrings["BHSCMS"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string qry = "Select RegistrationCode from [BHSCMS].[dbo].[RegistrationTable]";
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            conn.Open();
+
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                currentCode.Text = dr.GetValue(0).ToString();
+
+            }
+
+            conn.Close();
+
+        }
+
+        protected void submitCode_Click(object sender, EventArgs e)
+        {
+
+            string qry = "Update [BHSCMS].[dbo].[RegistrationTable]" + " Set RegistrationCode = @RegistrationCode" + " Where RegID=1";
+
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand noncmd = conn.CreateCommand();
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(qry, conn);
+
+                cmd.Parameters.Add("@RegistrationCode", SqlDbType.VarChar, 10, "RegistrationCode");
+
+                cmd.Parameters["@RegistrationCode"].Value = regCode.Text;
+                cmd.ExecuteNonQuery();
+            }
+
+            catch (SqlException ex)
+            {
+
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+
+    }
+}
